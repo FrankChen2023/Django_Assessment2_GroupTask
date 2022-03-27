9e891321985d80899d9720e1474c641ca24141bb
ZHAOQI CHEN <z.chen5.21@abdn.ac.uk>
2022-03-27 12:41:21 +0000

    First commit

--- CrimeDataSan/views.py
+++ CrimeDataSan/views.py
@@ -4,29 +4,29 @@ from .models import CrimeDate, CrimePosition
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'Crime/index.html')
 
 def date_search(request):
-    return render(request, 'date_search.html')
+    return render(request, 'Crime/date_search.html')
 
 def position_search(request):
-    return render(request, 'position_search.html')
+    return render(request, 'Crime/position_search.html')
 
 def coordinates_search(request, latitude, longitude):
     latitude = str(latitude)
     longitude = str(longitude)
     position_search_rows = CrimePosition.objects.filter(latitude__startwith=latitude, longitude__startwith=longitude)
-    return render(request, 'coordinates_search.html', {'position_search_rows' : position_search_rows})
+    return render(request, 'Crime/coordinates_search.html', {'position_search_rows' : position_search_rows})
 
 def date_result(request, date):
     Crime_date = CrimeDate.objects.get(date = date)
-    return render(request, 'date_result.html', {'Crime_date' : Crime_date})
+    return render(request, 'Crime/date_result.html', {'Crime_date' : Crime_date})
 
 def position_result(request, position):
     Crime_position = CrimePosition.objects.get(position = position)
-    return render(request, 'position_result.html', {'Crime_position' : Crime_position})
+    return render(request, 'Crime/position_result.html', {'Crime_position' : Crime_position})
 
 def total(request):
     Crime_total_date = CrimeDate.objects.all()
     Crime_total_position = CrimePosition.objects.all()
-    return render(request, 'position_result.html', {'Crime_total_date' : Crime_total_date, 'Crime_total_position' : Crime_total_position})
+    return render(request, 'Crime/position_result.html', {'Crime_total_date' : Crime_total_date, 'Crime_total_position' : Crime_total_position})
