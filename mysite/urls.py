94e266f535ebe20003c1cb7bbb255c2d8c359dc9
ZHAOQI CHEN <z.chen5.21@abdn.ac.uk>
2022-03-27 12:47:32 +0000

    First commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('CrimeDataSan.urls', namespace='Crime')),
+    path('', RedirectView.as_view(url='')),
 ]
